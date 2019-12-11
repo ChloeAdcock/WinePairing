d1ee60a693a16d046bc7951dd79f8d4d220d850d
Chloe Adcock <Chloe@Chloes-MacBook-Air.local>
2019-12-11 21:21:25 +0000

    User service

---
+++ src/main/java/com/qa/bae/service/UserService.java
@@ -0,0 +1,39 @@
+package com.qa.bae.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.qa.bae.domain.User;
+import com.qa.bae.repo.UserRepository;
+
+@Service
+public class UserService {
+	
+	private UserRepository userRepo;
+	
+	public UserService(UserRepository userRepo) {
+		this.userRepo = userRepo;
+	}
+	
+	public List<User> getAllUsers() {
+		if (userRepo.findAll().isEmpty()) {
+			setUpUsers();
+		}
+		return userRepo.findAll();
+	}
+	
+	private void setUpUsers() {
+		User user = new User();
+		userRepo.save(user);
+		}
+	
+	public User addNewUser(User user) {
+		return userRepo.save(user);
+	}
+	
+	public String deleteUser(Long id) {
+		userRepo.deleteById(id);
+		return "User succesfully deleted";
+	}
+}
