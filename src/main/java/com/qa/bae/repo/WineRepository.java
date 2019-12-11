b21c87b66f0c35bc3203aae04d63b63d6ac38257
Chloe Adcock <Chloe@Chloes-MacBook-Air.local>
2019-12-11 19:53:27 +0000

    Wine repo

--- src/main/java/com/qa/bae/repo/WineRepository.java
+++ src/main/java/com/qa/bae/repo/WineRepository.java
@@ -3,9 +3,9 @@ package com.qa.bae.repo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.qa.bae.domain.Food;
+import com.qa.bae.domain.Wine;
 
 @Repository
-public interface FoodRepository extends JpaRepository<Food, Long> {
+public interface WineRepository extends JpaRepository<Wine, Long> {
 
 }
