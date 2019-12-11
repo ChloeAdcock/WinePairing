38e7f886a7a30c2317cdc44bd177894b03760846
Chloe Adcock <Chloe@Chloes-MacBook-Air.local>
2019-12-11 19:12:20 +0000

    User repo

--- src/main/java/com/qa/bae/repo/UserRepository.java
+++ src/main/java/com/qa/bae/repo/UserRepository.java
@@ -1,11 +1,12 @@
 package com.qa.bae.repo;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.qa.bae.domain.Food;
+import com.qa.bae.domain.User;
 
 @Repository
-public interface FoodRepository extends JpaRepository<Food, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
 }
