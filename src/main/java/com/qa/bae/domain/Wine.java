bc6c2fdb2d3e342d2c3f3c36d49ad4bd14cf9bed
Chloe <Chloe@chloes-air>
2019-12-31 14:56:01 +0000

    App won't run

--- src/main/java/com/qa/bae/domain/Wine.java
+++ src/main/java/com/qa/bae/domain/Wine.java
@@ -6,6 +6,7 @@ import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
@@ -22,7 +23,8 @@ public class Wine {
 	private String tastingNotes;
 	private int likes;
 	
-    @OneToMany(mappedBy="wine")
+    @OneToMany
+    @JoinColumn(name="food_id")
     private Set<Food> food;
     
 	public Wine() {
