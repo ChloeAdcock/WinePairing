9c72807fad83f1936b3e8c871ac4e8a56aa44e50
Chloe <Chloe@chloes-air>
2019-12-31 13:25:14 +0000

    User controller

--- src/main/java/com/qa/bae/domain/Wine.java
+++ src/main/java/com/qa/bae/domain/Wine.java
@@ -96,4 +96,68 @@ public class Wine {
 		return "Wine [id=" + id + ", name=" + name + ", grape=" + grape + ", description=" + description
 				+ ", tastingNotes=" + tastingNotes + ", likes=" + likes + ", food=" + food + "]";
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		result = prime * result + ((food == null) ? 0 : food.hashCode());
+		result = prime * result + ((grape == null) ? 0 : grape.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + likes;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((tastingNotes == null) ? 0 : tastingNotes.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Wine other = (Wine) obj;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		}
+		else if (!description.equals(other.description))
+			return false;
+		if (food == null) {
+			if (other.food != null)
+				return false;
+		} 
+		else if (!food.equals(other.food))
+			return false;
+		if (grape == null) {
+			if (other.grape != null)
+				return false;
+		} 
+		else if (!grape.equals(other.grape))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} 
+		else if (!id.equals(other.id))
+			return false;
+		if (likes != other.likes)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} 
+		else if (!name.equals(other.name))
+			return false;
+		if (tastingNotes == null) {
+			if (other.tastingNotes != null)
+				return false;
+		} 
+		else if (!tastingNotes.equals(other.tastingNotes))
+			return false;
+		return true;
+	}
 }
