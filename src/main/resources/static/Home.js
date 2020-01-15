fe3c85473d015742cc5feebc1c930af16852194b
Chloe <Chloe@172.17.25.11>
2020-01-15 20:45:15 +0000

    input validation

--- src/main/resources/static/Home.js
+++ src/main/resources/static/Home.js
@@ -1,12 +1,98 @@
-function notEmpty(){
-    let nameInput = document.forms["wineForm"]["inputWineName"].value;
-    let text = document.getElementById("descriptionHelp").innerText;
-    if (nameInput == "") {
-        text = "Name must be filled out";
+const PORT = 8081;
+
+function wineRequired(){
+    let nameInput = document.getElementById("inputWineName");
+    let nameText = document.getElementById("wineNameHelp");
+    if (nameInput.value.length == 0) {
+        nameText.innerText = "Please enter a wine name";
+        nameInput.id = "required";
+        nameText.id = "requiredText";
+        return false;
     }
+    return true;
 }
 
-const PORT = 8081;
+function maxLengthWine() {
+    let nameInput = document.getElementById("inputWineName");
+    let grapeInput = document.getElementById("inputGrape");
+    let descriptionInput = document.getElementById("inputWineDescription");
+    let tastingNotesInput = document.getElementById("inputTastingNotes");
+
+    let nameText = document.getElementById("wineNameHelp");
+    let grapeText = document.getElementById("grapeHelp");
+    let descriptionText = document.getElementById("wineDescriptionHelp");
+    let tastingNotesText = document.getElementById("tastingNotesHelp");
+
+    let valid = true;
+
+    if (nameInput.value.length > 100) {
+        nameInput.id = "required";
+        nameText.id = "requiredText";
+        valid = valid && false;
+    }
+    
+    if (grapeInput.value.length > 100) {
+        grapeInput.id = "required";
+        grapeText.id = "requiredText";
+        valid = valid && false;
+    }
+    
+    if (descriptionInput.value.length > 100) {
+        descriptionInput.id = "required";
+        descriptionText.id = "requiredText";
+        valid = valid && false;
+    }
+    
+    if (tastingNotesInput.value.length > 100) {
+        tastingNotesInput.id = "required";
+        tastingNotesText.id = "requiredText";
+        valid = valid && false;
+    }
+    return valid;
+}
+
+function foodRequired(){
+    let nameInput = document.getElementById("inputFoodName");
+    let text = document.getElementById("foodNameHelp");
+    if (nameInput.value.length == 0) {
+        text.innerText = "Please enter a food name";
+        nameInput.id = "required";
+        text.id = "requiredText";
+        return false;
+    }
+    return true;
+}
+
+function maxLengthFood() {
+    let nameInput = document.getElementById("inputFoodName");
+    let allergensInput = document.getElementById("inputAllergens");
+    let descriptionInput = document.getElementById("inputFoodDescription");
+
+    let nameText = document.getElementById("foodNameHelp");
+    let allergensText = document.getElementById("allergensHelp");
+    let descriptionText = document.getElementById("foodDescriptionHelp");
+
+    let valid = true;
+
+    if (nameInput.value.length > 100) {
+        nameInput.id = "required";
+        nameText.id = "requiredText";
+        valid = valid && false;
+    }
+    
+    if (allergensInput.value.length > 100) {
+        allergensInput.id = "required";
+        allergensText.id = "requiredText";
+        valid = valid && false;
+    }
+    
+    if (descriptionInput.value.length > 100) {
+        descriptionInput.id = "required";
+        descriptionText.id = "requiredText";
+        valid = valid && false;
+    }
+    return valid;
+}
 
 function addWine() {
         let wineName = document.getElementById('inputWineName').value;
@@ -21,14 +107,16 @@ function addWine() {
             "tastingNotes": tastingNotes
         }
         
+        if (wineRequired() == true && maxLengthWine() == true) {
         axios.post("http://localhost:" + PORT + "/wine/addWine", data)     
         .then ((response) => {
             console.log("post wines request: " + data)
             location.reload();
         }).catch ((error) => {
             console.error(error);
-        }   
+        } 
  )
+}
 }
 
 let wines = [];
@@ -70,6 +158,7 @@ function addFood() {
         }
     }
     
+    if (foodRequired() == true && maxLengthFood() == true) {
     axios.post("http://localhost:" + PORT + "/food/addFood?id=" + winePairing, data)     
     .then ((response) => {
         console.log("post foods request: " + data)
@@ -78,5 +167,6 @@ function addFood() {
         console.error(error);
     }
 )}
+}
 
 
