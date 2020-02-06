03277e93f58f9d5e988181ba9bfba765d9248a49
ChloeAdcock <58259820+ChloeAdcock@users.noreply.github.com>
2020-02-06 14:32:34 +0000

    removed winepage ref

--- src/test/java/com/qa/bae/selenium/AddWineTest.java
+++ src/test/java/com/qa/bae/selenium/AddWineTest.java
@@ -15,7 +15,6 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.qa.bae.seleniumPages.HomeWine;
-import com.qa.bae.seleniumPages.WinePage;
 
 public class AddWineTest {
 	
