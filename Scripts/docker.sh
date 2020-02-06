6a0c6b8b1c8536c26972846a19eeb9f39f720061
ChloeAdcock <58259820+ChloeAdcock@users.noreply.github.com>
2020-02-06 17:24:59 +0000

    Update docker.sh

--- Scripts/docker.sh
+++ Scripts/docker.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-./delete.sh
-sudo apt update
-git clone https://github.com/ChloeAdcock/WinePairing.git 
-cd WinePairing
-git checkout Containerised 
+docker stop wine-app
+docker rm wine-app
+docker rmi wines
+sudo rm wine.jar
+wget --output-document wine.jar http://3.11.84.155:8081/service/rest/v1/search/assets/download?repository=wine-hosted\&group=com.qa.bae\&name=WinePairing\&0.0.1-SNAPSHOT\&sort=version\&maven.extension=jar
 docker build -t wines .
 docker run -dit --restart unless-stopped -d -p 9090:8081 --name wine-app wines:latest
+
